7b44f26553fe81285fad271c11035aecce7635d8
LiukenMonteiro <liuken37@gmail.com>
2025-05-21 15:22:25 -0300

    projeto convem + inicio config aws.ts e database.ts

---
+++ backend/src/config/aws.ts
@@ -0,0 +1,40 @@
+import {  DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { SQSClient } from '@aws-sdk/client-sqs';
+import { LambdaClient } from '@aws-sdk/client-lambda';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config = {
+    region: process.env.AWS_REGION || 'us-east-1',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    }
+};
+
+//Meu cliente aws
+const dynamoClient = new DynamoDBClient(config);
+const docClient = DynamoDBDocumentClient.from(dynamoClient);
+const sqsClient = new SQSClient(config);
+const lambdaClient = new LambdaClient(config);
+
+//tabelas DynamoDB
+const PIX_QR_CODES_TABLE = process.env.PIX_QR_CODES_TABLE || 'pixQrCodes';
+const PIX_CASH_OUT_TABLE = process.env.PIX_CASH_OUT_TABLE || 'pixCashOut';
+
+// filas SQS
+const CASH_IN_QUEUE_URL = process.env.CASH_IN_QUEUE_URL || '';
+const CASH_OUT_QUEUE_URL = process.env.CASH_OUT_QUEUE_URL || '';
+
+export {
+    docClient,
+    dynamoClient,
+    sqsClient,
+    lambdaClient,
+    PIX_QR_CODES_TABLE,
+    PIX_CASH_OUT_TABLE,
+    CASH_IN_QUEUE_URL,
+    CASH_OUT_QUEUE_URL
+}
---
+++ backend/src/config/database.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ASAAS_API_KEY = process.env.ASAAS_API_KEY || '';
+const ASAAS_BASE_URL = process.env.ASAAS_BASE_URL || 'https://sandbox.asaas.com/api/v3';
+
+export {
+    ASAAS_API_KEY,
+    ASAAS_BASE_URL
+}

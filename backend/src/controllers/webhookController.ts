4e4a5bdde9a15bc2fe30908e41ff7f0802363955
LiukenMonteiro <liuken37@gmail.com>
2025-05-22 08:31:45 -0300

    acoplação do status(200) e mudanças na formatação

--- backend/src/controllers/webhookController.ts
+++ backend/src/controllers/webhookController.ts
@@ -3,34 +3,37 @@ import { DynamoDBService } from "../services/dynamoDBService";
 import { SQSService } from "../services/sqsService";
 import { Request, Response } from "express";
 
-
 const dynamoDBService = new DynamoDBService();
 const sqsService = new SQSService();
 
 //webhook do cash in (recebendo dinheiro)
 export const receiveCashInWebhook = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const webhook = req.body;
+  try {
+    const webhook = req.body;
 
-        await sqsService.sendMessage(CASH_IN_QUEUE_URL,webhook); //enviando para a fila SQS
+    console.log('Webhook do Cash in recebido:', JSON.stringify(webhook)); // tipo cash in?
 
-        console.log('Webhook do Cash in recebido:', JSON.stringify(webhook));    
-    } catch (error) {
-        console.log('Erro ao processar o webhook do cash in:', error);
-        res.status(500).json({ error: 'Erro ao processar o webhook do cash in' });
-    }
+    await sqsService.sendMessage(CASH_IN_QUEUE_URL, webhook); //enviando para a fila SQS
+
+    res.status(200).json({ message: 'Webhook Cash in recebido e enviado para processamento' });
+  } catch (error) {
+    console.log('Erro ao processar o webhook do cash in:', error);
+    res.status(500).json({ error: 'Erro ao processar o webhook do cash in' });
+  }
 };
 
 //webhook do cash out (sacando dinheiro)
 export const receiveCashOutWebhook = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const webhook = req.body;
+  try {
+    const webhook = req.body;
 
-        await sqsService.sendMessage(CASH_IN_QUEUE_URL,webhook); //enviando para a fila SQS
+    console.log('Webhook do Cash out recebido:', JSON.stringify(webhook)); // tipo cash out?
 
-        console.log('Webhook do Cash out recebido:', JSON.stringify(webhook));    
-    } catch (error) {
-        console.log('Erro ao processar o webhook do cash out:', error);
-        res.status(500).json({ error: 'Erro ao processar o webhook do cash out' });
-    }
-}
+    await sqsService.sendMessage(CASH_IN_QUEUE_URL, webhook); //enviando para a fila SQS
+
+    res.status(200).json({ message: 'Webhook Cash out recebido e enviado para processamento' });
+  } catch (error) {
+    console.log('Erro ao processar o webhook do cash out:', error);
+    res.status(500).json({ error: 'Erro ao processar o webhook do cash out' });
+  }
+};

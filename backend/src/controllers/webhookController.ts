25c99b25577aa9e01a2efe5998755c3baaef2d1b
LiukenMonteiro <liuken37@gmail.com>
2025-05-22 07:33:02 -0300

    enviando o webhook pra fila sqs

--- backend/src/controllers/webhookController.ts
+++ backend/src/controllers/webhookController.ts
@@ -1,3 +1,4 @@
+import { CASH_IN_QUEUE_URL } from "../config/aws";
 import { DynamoDBService } from "../services/dynamoDBService";
 import { SQSService } from "../services/sqsService";
 import { Request, Response } from "express";
@@ -11,7 +12,7 @@ export const receiveCashInWebhook = async (req: Request, res: Response): Promise
     try {
         const webhook = req.body;
 
-        
+        await sqsService.sendMessage(CASH_IN_QUEUE_URL,webhook); //enviando o webhook para a fila SQS
 
         console.log('Webhook do Cash in recebido:', JSON.stringify(webhook));    
     } catch (error) {
